8a878f9d65593038745a98aa435719e062e672c4
Robin Gesell <robin.gesell@bayer.com>
2024-10-11 10:56:05 +0200

    Intial Commit

---
+++ DocFxTest/TestControl.xaml.cs
@@ -0,0 +1,39 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System.Windows.Controls;
+
+namespace DocFxTest
+{
+    /// <summary>
+    /// Interaction logic for TestControl.xaml
+    /// </summary>
+    [ObservableObject]
+    public partial class TestControl : UserControl
+    {
+        private const string _textA = "A";
+        private const string _textB = "B";
+
+        /// <summary>
+        /// The button text.
+        /// </summary>
+        [ObservableProperty]
+        private string _buttonText = _textA;
+
+        /// <summary>
+        /// The button command.
+        /// </summary>
+        [RelayCommand]
+        private void ButtonClick()
+        {
+            ButtonText = ButtonText == _textA ? _textB : _textA;
+        }
+
+        /// <summary>
+        /// The constructor.
+        /// </summary>
+        public TestControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
